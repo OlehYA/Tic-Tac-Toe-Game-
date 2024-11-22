b2287ddfc5bd77b8c66f13bc2f0661d0c35805c5
OlehYa <olehyakuts@gmail.com>
2024-11-22 16:25:43 +0200

    Created Animations for X and O

--- Tic-Tac-Toe Game/MainWindow.xaml.cs
+++ Tic-Tac-Toe Game/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace Tic_Tac_Toe_Game
 {
@@ -24,6 +25,12 @@ namespace Tic_Tac_Toe_Game
             {Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png"))},
         };
 
+        private readonly Dictionary<Player, ObjectAnimationUsingKeyFrames> animations = new()
+        {
+            {Player.X, new ObjectAnimationUsingKeyFrames() },
+            {Player.O, new ObjectAnimationUsingKeyFrames() }
+        };
+
         private readonly Image[,] imageControls = new Image[3, 3];
         private readonly GameState gameState = new GameState();
 
@@ -31,6 +38,7 @@ namespace Tic_Tac_Toe_Game
         {
             InitializeComponent();
             SetupGameGrid();
+            SetupAnimations();
 
             gameState.MoveMade += OnMoveMade;
             gameState.GameEnded += OnGameEnded;
@@ -50,11 +58,31 @@ namespace Tic_Tac_Toe_Game
             }
         }
 
+        private void SetupAnimations()
+        {
+            animations[Player.X].Duration = TimeSpan.FromSeconds(.25);
+            animations[Player.O].Duration = TimeSpan.FromSeconds(.25);
+
+            for(int i =0; i< 16; i++)
+            {
+                Uri xUri = new Uri($"pack://application:,,,/Assets/X{i}.png");
+                BitmapImage xImg = new BitmapImage(xUri);
+                DiscreteObjectKeyFrame xKeyFrame = new DiscreteObjectKeyFrame(xImg);
+                animations[Player.X].KeyFrames.Add(xKeyFrame);
+
+
+                Uri oUri = new Uri($"pack://application:,,,/Assets/O{i}.png");
+                BitmapImage oImg = new BitmapImage(oUri);
+                DiscreteObjectKeyFrame oKeyFrame = new DiscreteObjectKeyFrame(oImg);
+                animations[Player.O].KeyFrames.Add(oKeyFrame);
+            }
+        }
+
         private void TransitionToEndScreen(string text, ImageSource winnerImage)
         {
             TurnPanel.Visibility = Visibility.Hidden;
             GameCanvas.Visibility = Visibility.Hidden;
-            ResultText.Text= text;
+            ResultText.Text = text;
             WinnerImage.Source = winnerImage;
             EndScreen.Visibility = Visibility.Visible;
         }
@@ -62,15 +90,50 @@ namespace Tic_Tac_Toe_Game
         private void TransitionToGameScreen()
         {
             EndScreen.Visibility = Visibility.Hidden;
-            TurnPanel.Visibility= Visibility.Visible;
+            Line.Visibility = Visibility.Hidden;
+            TurnPanel.Visibility = Visibility.Visible;
             GameCanvas.Visibility = Visibility.Visible;
 
+        }
+
+        private (Point, Point) FindLinePoints(WinInfo winInfo)
+        {
+            double squareSize = GameGrid.Width / 3;
+            double margin = squareSize / 2;
+
+            if (winInfo.Type == WinType.Row)
+            {
+                double y = winInfo.Number * squareSize + margin;
+                return (new Point(0, y), new Point(GameGrid.Width, y));
+            }
+            if (winInfo.Type == WinType.Column)
+            {
+                double x = winInfo.Number * squareSize + margin;
+                return (new Point(0, x), new Point(x, GameGrid.Height));
+            }
+            if (winInfo.Type == WinType.MainDiagonal)
+            {
+                return (new Point(0, 0), new Point(GameGrid.Width, GameGrid.Height));
+            }
+
+            return (new Point(GameGrid.Width, 0), new Point(0, GameGrid.Height));
+        }
+
+        private void ShowLine(WinInfo winInfo)
+        {
+            (Point start, Point end) = FindLinePoints(winInfo);
+
+            Line.X1 = start.X;
+            Line.Y1 = start.Y;
+
+            Line.X2 = end.X;
+            Line.Y2 = end.Y;
         }
 
         private void OnMoveMade(int r, int c)
         {
             Player player = gameState.GameGrid[r, c];
-            imageControls[r, c].Source = imageSources[player];
+            imageControls[r, c].BeginAnimation(Image.SourceProperty, animations[player]);
             PlayerImage.Source = imageSources[gameState.CurrentPlayer];
         }
 
@@ -78,21 +141,23 @@ namespace Tic_Tac_Toe_Game
         {
             await Task.Delay(1000);
 
-            if(gameResult.Winner == Player.None)
+            if (gameResult.Winner == Player.None)
             {
                 TransitionToEndScreen("Its a tie!,", null);
             }
             else
             {
-                TransitionToEndScreen("Winner:", imageSources[gameResult.Winner]);  
+                ShowLine(gameResult.WinInfo);
+                await Task.Delay(1000);
+                TransitionToEndScreen("Winner:", imageSources[gameResult.Winner]);
             }
         }
 
         private void OnGameRestarted()
         {
-            for(int r =0; r<3; r++)
+            for (int r = 0; r < 3; r++)
             {
-                for(int c =0; c<3; c++)
+                for (int c = 0; c < 3; c++)
                 {
                     imageControls[r, c].Source = null;
                 }
